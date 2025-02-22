5a2a44ffa168eb8e021d69e7ff4f3934676f1e28
issaahr <isabeli.romano@gmail.com>
2025-02-22 01:25:09 -0300

    feat: configure Swagger and add test route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config';
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import { swaggerConfig } from './config/swagger';
+import routes from './routes/routes';
 
 const app = express();
-const port = 4444;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port} `);
+const swaggerSpec = swaggerJSDoc(swaggerConfig);
+
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.get('/swagger.json', (_req, res) => res.send(swaggerSpec));
+
+console.log(`Add swagger on /swagger`);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server listening on port ${process.env.SERVER_PORT}`);
 });
